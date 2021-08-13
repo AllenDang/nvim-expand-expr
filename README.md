ed52f04e61ac1c19ce37e500aec5da37b793eda7
AllenDang <allengnr@gmail.com>
2021-08-13 15:50:55 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # nvim-expand-expr
+
 Expand and repeat expression to multiple lines for neovim.
+
+# Requirements
+
+`Neovim >= 0.5`
+
+# Installation
+
+[packer.nvim](https://github.com/wbthomason/packer.nvim)
+`use 'AllenDang/nvim-expand-expr'`
+
+# Usage
+
+```lua
+require("nvim-expand-expr").expand()
+```
+
+It will parse cursor line and expand it base on below syntax.
+
+`range|expr`
+
+## range
+
+`range` has two forms.
+
+1. Single number
+   `10|print({%d})` will expand as
+
+```
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+```
+
+2. Range format
+   `3,7|print({%d})` will expand as
+
+```
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+```
+
+## expr
+
+_expr_ is a string contains multiple `{%d}` parts, the content inside {} will be eval as lua expression.
+
+`3|{%d} + {%d+1} + {%d+2}` will expand as
+
+```
+1 + 2 + 3
+2 + 3 + 4
+3 + 4 + 5
+```
+
+`3|{%d .. ' hello world ' .. %d*3}`
+
+```
+1 hello world 3
+2 hello world 6
+3 hello world 9
+```
---
+++ lua/expand_expr.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+local function regex_escape(str)
+  return string.gsub(str, "[%(%)%.%%%+%-%*%?%[%^%$%]]", "%%%1")
+end
+
+local replace = function(str, this, that)
+  return string.gsub(str, regex_escape(this), that)
+end
+
+-- Syntax is range|expr, %d inside expr will be replaced with range index
+-- range: 10|... or 1,10|...
+-- expr: {%d} or {%d+1}
+-- anything inside {} is a lua expression
+function M.expand_expr()
+  local expr = vim.api.nvim_get_current_line()
+  local pos = string.find(expr, "|")
+  local range = string.sub(expr, 1, pos - 1)
+  local content = string.sub(expr, pos + 1)
+
+  if #range == 0 and #content == 0 then
+    print("invalid expandable expr")
+    return
+  end
+
+  -- if range contains ,
+  local from = 1
+  local to = 1
+
+  local has_comma = string.find(range, ",")
+  if has_comma == nil then
+    to = tonumber(range)
+  else
+    local range_parts = vim.split(range, ",")
+    if #range_parts > 2 then
+      print("range part could only contains one comma")
+      return
+    end
+
+    from = tonumber(range_parts[1])
+    to = tonumber(range_parts[2])
+  end
+
+  if from == nil or to == nil then
+    print("range part should be number")
+    return
+  end
+
+  -- parse expression in side {}
+  local expr_parts = {}
+  for e in string.gmatch(content, "{(.-)}") do
+    table.insert(expr_parts, {
+      original = "{" .. e .. "}",
+      expression = e,
+    })
+  end
+
+  local lines = { "" }
+  for i = from, to do
+    local temp_line = content
+    -- eval expression parts
+    for _, ep in ipairs(expr_parts) do
+      local e = replace(ep.expression, "%d", i)
+      local value = vim.api.nvim_eval(e)
+
+      temp_line = replace(temp_line, ep.original, value)
+    end
+
+    table.insert(lines, temp_line)
+  end
+
+  vim.api.nvim_del_current_line()
+  vim.api.nvim_put(lines, "", true, true)
+end
+
+return M
